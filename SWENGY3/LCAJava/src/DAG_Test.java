56a81e33448a378a115b25bd8607a553c8550572
Maria Cairns <78805824+cairns-m@users.noreply.github.com>
2021-11-02 09:12:51 +0000

    Fixed error in Test

--- SWENGY3/LCAJava/src/DAG_Test.java
+++ SWENGY3/LCAJava/src/DAG_Test.java
@@ -6,17 +6,31 @@ import org.junit.Test;
 
 public class DAG_Test {
 
+
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-    @Test
 	public void testIfEmpty() {
 		DAG dag = new DAG(10);
 		assertEquals(-1, dag.findLCA(2,3));
 	}
+	@Test
+	public void testLCA()
+	{
+		DAG dag = new DAG(10);
 
-    
+		dag.addEdge(0, 3);                  //     0
+		dag.addEdge(0, 5);                  //    / \
+		dag.addEdge(2, 7);                  //   2   5
+		dag.addEdge(5, 7);                  //    \ / \
+		dag.addEdge(7, 8);                  //     7   3
+		dag.addEdge(5, 9);                  //     |   |
+		dag.addEdge(9, 6);                  //     8   6
+
+		assertEquals(0, dag.findLCA(0, 3));
+		assertEquals(5, dag.findLCA(8, 6));
+		assertEquals(5, dag.findLCA(7, 6));
+	}	
+
 	@Test
 	public void addAnEdge()
 	{
@@ -27,7 +41,7 @@ public class DAG_Test {
 
 		// edge will not be added as 13 is greater than 10
 
-		assertEquals(1, dag.Edge());
+		assertEquals(1, dag.E());
 	}	
 
 
@@ -35,7 +49,7 @@ public class DAG_Test {
 	public void testVertex()
 	{
 		DAG dag = new DAG(10);
-		assertEquals(10, dag.Vertex());
+		assertEquals(10, dag.V());
 		// test for negative
 		assertEquals(-1, dag.findLCA(-7, 1));
 		// test for non present
@@ -51,7 +65,7 @@ public class DAG_Test {
 		dag.addEdge(2, 3);
 		dag.addEdge(4, 4);
 
-		assertEquals(3, dag.Edge());
+		assertEquals(3, dag.E());
 	}
 
 	@Test
